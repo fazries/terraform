a9ae20ca559edd23b02ecb94f0b484702d4f4b50
Fazrie Setyadi <setyadi.wijaya@traveloka.com>
2017-07-31 19:09:19 +0700

    adding module for security group

---
+++ securitygroup_module/base/variables.tf
@@ -0,0 +1,8 @@
+variable "Lst_cidr_blocks" {}
+variable "Str_vpc_id" {}
+variable "Str_secgroup_name" {}
+variable "Str_protocol" {}
+variable "Lst_from_port" {}
+variable "Lst_to_port" {}
+variable "Bol_self" {}
+variable "Str_num_of_secgroup" {}
