bfe2f7828084737c32ddfce91e00a5e7aa42a046
Fazrie Setyadi <setyadi.wijaya@traveloka.com>
2018-03-14 19:21:07 +0700

    test

--- ec2_module/base/output.tf
+++
@@ -1,4 +0,0 @@
-// Output the ID of the instance created
-   output "ec2_instance_id" {
-    value = "${aws_instance.ec2_instance.id}"
-}
--- main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2_instance" {
-	   source = "ec2_module/base"
-	   #Str_instance_type = "${var.Str_instance_type}"
-	   Str_instance_name = "${var.instance_name}"
-           #Map_ami_id	     = "${var.Map_ami_id}"
-	   Str_subnet_id     = "${var.subnet_id}"
-           Str_num_instance = "${var.num_instance}"
-	}
